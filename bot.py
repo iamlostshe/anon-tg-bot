dfbddd4804f8e39fc24e51b0c616324f59bf9ce3
ivan <vanamelcikov7275@gmail.com>
2025-03-21 23:52:46 +0400

    Refactor code style

--- bot.py
+++ bot.py
@@ -1,61 +1,69 @@
-# Встроенные модули
+"""Модуль взаимодействия с ботом."""  # noqa: RUF002
+
 import asyncio
 from os import getenv
 
-# Не встроенные модули
-from dotenv import load_dotenv
-from loguru import logger
-
-# Aiogram
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from dotenv import load_dotenv
+from loguru import logger
 
-# Мои модули (локальные)
 import messages
-
-# TODO дописать синхронизацию с бд
-# для сохранения в памяти анонимных сообщений
-
-# import db
 
 # Получам токен из .env
 load_dotenv()
+
 TOKEN = getenv("TOKEN")
-ADMINS_ID = getenv("ADMINS_ID").split(', ')
+ADMINS_ID = getenv("ADMINS_ID").replace(" ", "").split(",")
 
 # Инициализируем бота и диспетчер
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Отвечаем на приветственное сообщение (/start)
 @dp.message(CommandStart())
-async def command_start_handler(msg: Message) -> None:
+async def command_start(msg: Message) -> None:
+    """Отвечаем на приветственное сообщение (/start)."""
     # Отвечаем пользователю
     await msg.answer(messages.start_msg(msg.from_user.first_name))
-    # TODO Записываем пользователя в базу данных
-    #db.add_user(msg.from_user.id)
-    # Выводим информацию в консоль (лог)
-    logger.info('/start id={}, username={}, url={}', msg.from_user.id, msg.from_user.username, msg.from_user.username)
 
-# Отвечаем на любое другое сообщение
+    # Выводим информацию в консоль (лог)
+    logger.info(
+        "/start id={}, username={}, url={}",
+        msg.from_user.id,
+        msg.from_user.username,
+        msg.from_user.username,
+    )
+
 @dp.message()
-async def echo_handler(msg: Message) -> None:
+async def any_message(msg: Message) -> None:
+    """Отвечаем на любое другое сообщение."""
     # Отвечаем пользователю
     await msg.answer(messages.success(msg.from_user.first_name))
+
     # Пройдемся по всем админам
-    for id in ADMINS_ID:
-        # Отправляем сообщение об отправителе админу канала
-        #await bot.send_message(id, messages.info(msg.from_user.id, msg.from_user.username, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.language_code))
+    for admin_id in ADMINS_ID:
         # Отправляем текст сообщения админу
-        await bot.send_message(id, msg.text)
+        await bot.send_message(admin_id, msg.text)
+
     # Выводим информацию в консоль (лог)
-    logger.info('[m] id={}, username={}, text={}, url=https://t.me/{}', msg.from_user.id, msg.from_user.username, msg.text, msg.from_user.username)
+    logger.info("[m] id={}, username={}, text={}, url=https://t.me/{}",
+        msg.from_user.id,
+        msg.from_user.username,
+        msg.text, msg.from_user.username,
+    )
 
 
 # Запускаем бота
 async def main() -> None:
-    logger.info('Bot start polling...')
+    """Скрипт инициализации и запуска бота."""
+    # Устанавливаем файл для сбора логов  # noqa: RUF003
+    logger.add("anon-tg-bot.log")
+
+    # Выводим лог
+    logger.info("Bot start polling...")
+
+    # Запускаем бота
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
--- messages.py
+++ messages.py
@@ -1,24 +1,41 @@
+"""Модуль статических заготовленных сообщений."""
+
+from __future__ import annotations
+
+
+def select_lang(lang_code: str | None) -> str:
+    """Вспомогательная функция; определяет язык ответа."""
+    # Определяем язык ответа
+    if not lang_code:
+        return "ru"
+
+    return "ru" if lang_code == "ru" else "en"
+
+
 def start_msg(first_name: str, lang_code: str | None = None) -> str:
-    return f'''Приветствую, {first_name}!
-    
-Для того чтобы отправить мне анонимное сообщение просто напиши боту)'''
+    """Ответ на приветственное сообщение."""
+    # Возвращаем текст ответа
+    return {
+        "ru": (
+            f"Приветствую, {first_name}!\n\n"
+            "Для того чтобы отправить мне анонимное сообщение просто напиши боту)"
+        ),
+        "en": (
+            f"Hello, {first_name}!\n\n."
+            "To send me an anonymous message, just message the bot."
+        ),
+    }[select_lang(lang_code)]
 
-def success(first_name, lang_code: str | None = None) -> str:
-    return f'''Спасибо за вопросик, {first_name})
-    
-В скором времени постараюсь ответить на него в своем канале!'''
-
-def info(user_id: str, username: str, first_name: str, last_name: str, lang_code: str | None = None):
-    return f'''Новое сообщение!
-    
-Информация об отправителе:
-
-id: {user_id};
-username: {username};
-first_name: {first_name};
-last_name: {last_name};
-
-url (если указан username): https://t.me/{username}
-
-Письмо отправлено отдельным сообщением ниже:
-'''
+def success(first_name: str, lang_code: str | None = None) -> str:
+    """Ответ на сообщение-анонимный вопрос."""
+    # Возвращаем текст ответа
+    return {
+        "ru": (
+            f"Спасибо за вопросик, {first_name})\n\n"
+            "В скором времени постараюсь ответить на него!"  # noqa: RUF001
+        ),
+        "en": (
+            "Thank you for the question, {first_name})\n\n"
+            "I'll try to answer it soon!"
+        ),
+    }[select_lang(lang_code)]
